27f3b4b584c341cd2ef7bb5c4bff7050ec01eef6
Ivan Dmitriev <dmitrievstekloy@gmail.com>
2020-06-01 20:08:31 +0300

    Post manager added

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,17 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] post;
+
+    public Post[] postSearch() {
+
+        // TODO: add logic
+        return null;
+    }
+
+    public void postDelete() {
+        // TODO: add logic
+    }
+}
